cbbce755d0d9b0121f934020f25f18fcad255b02
Nirbhay <nirbhaysingh943012@gmail.com>
2023-07-27 23:33:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./component/Header";
 
 function App() {
+  const [num, setNum] = useState(1);
+  function inc() {
+    setNum(num+1);
+  }
+
+  function dec() {
+    setNum(num-1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="main">
+        <h1 className="heading">{num}</h1>
+        <div className="buttons">
+          <button onClick={inc}>Increment</button>
+          <button onClick={dec}>deccrement</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>useState Hook</h1>
+    </div>
+  )
+}
+
+export default Header
